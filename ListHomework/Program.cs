2aaf372b7f6cb7f9ea709fdd7c04785a36018748
ESchulstad <eschulstad@gmail.com>
2016-09-19 19:25:00 -0400

    Initial Commit

---
+++ ListHomework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<bool> boolList = new List<bool>() { true, false, false, true, false };
+
+            foreach(bool answer in boolList)
+            {
+                if(answer == true)
+                {
+                    Console.WriteLine("Better bring an umbrella.");
+                }
+                else if(answer == false)
+                {
+                    Console.WriteLine("No rain today, enjoy the sun!");
+                }
+
+            }
+
+        }
+    }
+}
